9bf9d7d1c259ea1ddf0b8783f9b6bcce761fc709
Sergio <sjimenez@enac.es>
2020-10-13 16:32:22 +0200

    Primer switch para probar varios casos

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,8 +14,22 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Prueba de cambio para ver en teams: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            var contenido = turnContext.Activity.Text;
+            var respuesta = "";
+            switch (contenido)
+            {
+                case "hola":
+                    respuesta = "Hola trabajador de ENAC, eres un privilegiado y yo un simple bot.";
+                    break;
+                case "adios":
+                    respuesta = "Hasta luego, no me preguntes más, cansino.";
+                    break;
+                default:
+                    respuesta = "No contemplo esa pregunta/frase. Mala suerte.";
+                    break;
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(respuesta, respuesta), cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
